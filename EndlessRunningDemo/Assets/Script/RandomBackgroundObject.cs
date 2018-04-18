0264c346c126b44a636e36c60a5b1c9fa255abe8
Frank-Zou <zyswwang@qq.com>
2018-04-18 17:15:54 +0800

    update

--- EndlessRunningDemo/Assets/Script/RandomBackgroundObject.cs
+++ EndlessRunningDemo/Assets/Script/RandomBackgroundObject.cs
@@ -7,108 +7,127 @@ public class RandomBackgroundObject : MonoBehaviour
     private GameObject[] groupPool1;
     private GameObject[] groupPool2;
     private GameObject[] groupPool3;
-    private GameObject[] objectPool1;
-    private GameObject[] objectPool2;
-    private GameObject[] objectPool3;
-    private GameObject[] computerpool1;
-    private GameObject[] computerpool2;
-    private GameObject[] computerpool3;
-    private GameObject[] flowerPool;
-
+    private GameObject[] groupPool4;
+    private GameObject[] groupPool5;
 
     private int poolsize = 3;
     private int currentColumn = 0;
-    private float timeSinceLastSpawned;
-    private float spawnXposition = 14.0f;
+    private int groupLastNum;
+    private float timeSinceLastSpawned2;
+    private float groupspawnXposition = 19.0f;
     private Vector2 objectPoolPosition = new Vector2(-15.0f, -25.0f);
 
     public GameObject group1;
     public GameObject group2;
     public GameObject group3;
-    public GameObject object1;
-    public GameObject object2;
-    public GameObject object3;
-    public GameObject computer1;
-    public GameObject computer2;
-    public GameObject computer3;
-    public GameObject flower;
+    public GameObject group4;
+    public GameObject group5;
 
-    public int objectNumber = 2;
-    public float spawnRate = 8.0f;
-    public float objectMin = 2.0f;
-    public float objectMax = 4.0f;
+    public int groupObjectNumber = 2;
+    public float groupSpawnRate = 12.0f;
 
     void Start()
     {
         groupPool1 = new GameObject[poolsize];
         groupPool2 = new GameObject[poolsize];
         groupPool3 = new GameObject[poolsize];
-        objectPool1 = new GameObject[poolsize];
-        objectPool2 = new GameObject[poolsize];
-        objectPool3 = new GameObject[poolsize];
-        computerpool1 = new GameObject[poolsize];
-        computerpool2 = new GameObject[poolsize];
-        computerpool3 = new GameObject[poolsize];
-        flowerPool = new GameObject[poolsize];
+        groupPool4 = new GameObject[poolsize];
+        groupPool5 = new GameObject[poolsize];
 
         for (int i = 0; i < poolsize; i++)
         {
             groupPool1[i] = Object.Instantiate(group1, objectPoolPosition, Quaternion.identity);
             groupPool2[i] = Object.Instantiate(group2, objectPoolPosition, Quaternion.identity);
             groupPool3[i] = Object.Instantiate(group3, objectPoolPosition, Quaternion.identity);
-            objectPool1[i] = Object.Instantiate(object1, objectPoolPosition, Quaternion.identity);
-            objectPool2[i] = Object.Instantiate(object2, objectPoolPosition, Quaternion.identity);
-            objectPool3[i] = Object.Instantiate(object3, objectPoolPosition, Quaternion.identity);
-            computerpool1[i] = Object.Instantiate(computer1, objectPoolPosition, Quaternion.identity);
-            computerpool2[i] = Object.Instantiate(computer2, objectPoolPosition, Quaternion.identity);
-            computerpool3[i] = Object.Instantiate(computer3, objectPoolPosition, Quaternion.identity);
-            flowerPool[i] = Object.Instantiate(flower, objectPoolPosition, Quaternion.identity);
+            groupPool4[i] = Object.Instantiate(group3, objectPoolPosition, Quaternion.identity);
+            groupPool5[i] = Object.Instantiate(group3, objectPoolPosition, Quaternion.identity);
         }
     }
 
     void Update()
     {
-        timeSinceLastSpawned += Time.deltaTime;
+        timeSinceLastSpawned2 += Time.deltaTime;
 
-        if (GameControl.instance.isOver == false && timeSinceLastSpawned >= spawnRate)
+        if(GameControl.instance.isOver == false && timeSinceLastSpawned2 >= groupSpawnRate)
         {
-            int randomNum = Random.Range(1, objectNumber + 1);
-            timeSinceLastSpawned = 0;
-            if(randomNum == 1)
+            int randomNum = Random.Range(1, groupObjectNumber + 1);
+            timeSinceLastSpawned2 = 0;
+            RandomGroupObject(randomNum);
+        }
+    }
+
+    private void RandomGroupObject(int randomNum)
+    {
+        if (randomNum == 1)
+        {
+            if (groupLastNum == randomNum)
             {
-                ObjectSpawned(objectPool1);
-            }else if(randomNum == 2)
-            {
-                ComputerSpawned(computerpool1);
-            }else if(randomNum == 3)
-            {
-                ComputerSpawned(computerpool2);
-            }else if(randomNum == 4)
-            {
-                ComputerSpawned(computerpool3);
-            }else if(randomNum == 5)
-            {
-                FlowerSpawned(flowerPool);
-            }else if(randomNum == 6)
-            {
-                FlowerSpawned(computerpool2);
-            }else if(randomNum == 7)
+                randomNum += 1;
+                RandomGroupObject(randomNum);
+            }
+            else
             {
                 GroupSpawned(groupPool1);
-            }else if(randomNum == 8)
-            {
-                GroupSpawned(groupPool2);
-            }else if(randomNum == 9)
-            {
-                GroupSpawned(groupPool3);
+                groupLastNum = 1;
             }
         }
-    }
+        else if (randomNum == 2)
+        {
+            if (groupLastNum == randomNum)
+            {
+                randomNum += 1;
+                RandomGroupObject(randomNum);
+            }
+            else
+            {
+                GroupSpawned(groupPool2);
+                groupLastNum = 2;
+            }
+        }
+        else if (randomNum == 3)
+        {
+            if (groupLastNum == randomNum)
+            {
+                randomNum += 1;
+                RandomGroupObject(randomNum);
+            }
+            else
+            {
+                GroupSpawned(groupPool3);
+                groupLastNum = 3;
+            }
+        }
+        else if (randomNum == 4)
+        {
+            if (groupLastNum == randomNum)
+            {
+                randomNum += 1;
+                RandomGroupObject(randomNum);
+            }
+            else
+            {
+                GroupSpawned(groupPool4);
+                groupLastNum = 4;
+            }
+        }
+        else if (randomNum == 5)
+        {
+            if (groupLastNum == randomNum)
+            {
+                randomNum = 1;
+                RandomGroupObject(randomNum);
+            }
+            else
+            {
+                GroupSpawned(groupPool5);
+                groupLastNum = 5;
+            }
+        }
+}
 
-    public void ObjectSpawned(GameObject[] objectPool)
+    public void GroupSpawned(GameObject[] groupPool)
     {
-        float spawnYposition = Random.Range(objectMin, objectMax);
-        objectPool[currentColumn].transform.position = new Vector2(spawnXposition, spawnYposition);
+        groupPool[currentColumn].transform.position = new Vector2(groupspawnXposition, 0f);
         currentColumn++;
         if (currentColumn >= poolsize)
         {
@@ -116,33 +135,116 @@ public class RandomBackgroundObject : MonoBehaviour
         }
     }
 
-    public void ComputerSpawned(GameObject[] computerPool)
-    {
-        computerPool[currentColumn].transform.position = new Vector2(spawnXposition, 2.9f);
-        currentColumn++;
-        if (currentColumn >= poolsize)
-        {
-            currentColumn = 0;
-        }
-    }
+    //private void RandomObject(int randomNum)
+    //{
+    //    if (randomNum == 1)
+    //    {
+    //        if (lastNum == randomNum)
+    //        {
+    //            randomNum += 1;
+    //            RandomObject(randomNum);
+    //        }
+    //        else
+    //        {
+    //            ObjectSpawned(objectPool1);
+    //            lastNum = 1;
+    //        }
+    //    }
+    //    else if (randomNum == 2)
+    //    {
+    //        if (lastNum == randomNum)
+    //        {
+    //            randomNum += 1;
+    //            RandomObject(randomNum);
+    //        }
+    //        else
+    //        {
+    //            ComputerSpawned(computerpool1);
+    //            lastNum = 2;
+    //        }
+    //    }
+    //    else if (randomNum == 3)
+    //    {
+    //        if (lastNum == randomNum)
+    //        {
+    //            randomNum += 1;
+    //            RandomObject(randomNum);
+    //        }
+    //        else
+    //        {
+    //            ComputerSpawned(computerpool2);
+    //            lastNum = 3;
+    //        }
+    //    }
+    //    else if (randomNum == 4)
+    //    {
+    //        if (lastNum == randomNum)
+    //        {
+    //            randomNum += 1;
+    //            RandomObject(randomNum);
+    //        }
+    //        else
+    //        {
+    //            ComputerSpawned(computerpool3);
+    //            lastNum = 4;
+    //        }
+    //    }
+    //    else if (randomNum == 5)
+    //    {
+    //        if (lastNum == randomNum)
+    //        {
+    //            randomNum += 1;
+    //            RandomObject(randomNum);
+    //        }
+    //        else
+    //        {
+    //            FlowerSpawned(flowerPool);
+    //            lastNum = 5;
+    //        }
+    //    }
+    //    else if (randomNum == 6)
+    //    {
+    //        if (lastNum == randomNum)
+    //        {
+    //            randomNum = 1;
+    //            RandomObject(randomNum);
+    //        }
+    //        else
+    //        {
+    //            FlowerSpawned(computerpool2);
+    //            lastNum = 6;
+    //        }
+    //    }
+    //}
 
-    public void  GroupSpawned(GameObject[] groupPool)
-    {
-        groupPool[currentColumn].transform.position = new Vector2(spawnXposition, 0f);
-        currentColumn++;
-        if (currentColumn >= poolsize)
-        {
-            currentColumn = 0;
-        }
-    }
+    //public void ObjectSpawned(GameObject[] objectPool)
+    //{
+    //    float spawnYposition = Random.Range(objectMin, objectMax);
+    //    objectPool[currentColumn].transform.position = new Vector2(spawnXposition, spawnYposition);
+    //    currentColumn++;
+    //    if (currentColumn >= poolsize)
+    //    {
+    //        currentColumn = 0;
+    //    }
+    //}
 
-    public void FlowerSpawned(GameObject[] flowerPool)
-    {
-        flowerPool[currentColumn].transform.position = new Vector2(spawnXposition,2);
-        currentColumn++;
-        if(currentColumn >= poolsize)
-        {
-            currentColumn = 0;
-        }
-    }
+    //public void ComputerSpawned(GameObject[] computerPool)
+    //{
+    //    computerPool[currentColumn].transform.position = new Vector2(spawnXposition, 2.9f);
+    //    currentColumn++;
+    //    if (currentColumn >= poolsize)
+    //    {
+    //        currentColumn = 0;
+    //    }
+    //}
+
+    //public void FlowerSpawned(GameObject[] flowerPool)
+    //{
+    //    flowerPool[currentColumn].transform.position = new Vector2(spawnXposition,2);
+    //    currentColumn++;
+    //    if(currentColumn >= poolsize)
+    //    {
+    //        currentColumn = 0;
+    //    }
+    //}
 }
