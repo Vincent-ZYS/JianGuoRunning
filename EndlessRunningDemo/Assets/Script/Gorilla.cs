1d7f0643f0db55543ac8864387976707ff106c7f
Frank-Zou <zyswwang@qq.com>
2018-04-27 16:34:15 +0800

    Update
    修复猩猩遁地；新添猩猩抓取动画

--- EndlessRunningDemo/Assets/Script/Gorilla.cs
+++ EndlessRunningDemo/Assets/Script/Gorilla.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gorilla : MonoBehaviour {
-    
+
+    public Animator gorillaAnimator;
     public Transform gorillaHidePositon;
     public Transform gorillaDestinationPosition;
 
@@ -22,7 +23,11 @@ public class Gorilla : MonoBehaviour {
             gorillaTransform.position = Vector2.MoveTowards(gorillaTransform.position, gorillaHidePositon.position, Time.deltaTime * gorillaSpeed);
         }else if(GameControl.instance.isOver == true)
         {
-            gorillaTransform.position = Vector2.MoveTowards(gorillaTransform.position, gorillaDestinationPosition.position - new Vector3(1,0), Time.deltaTime * gorillaSpeed * 5.0f);
+            gorillaTransform.position = Vector2.MoveTowards(gorillaTransform.position, gorillaDestinationPosition.position - new Vector3(1, -0.5f), Time.deltaTime * gorillaSpeed * 5.0f);
+            if(Vector2.Distance(transform.position,gorillaDestinationPosition.position) <= 2.0f)
+            {
+                gorillaAnimator.SetTrigger("Grab");
+            }
         }
 	}
 }
